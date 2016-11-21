5e575522797cf8f82af290b0ad0189412d74e15d
louis del rio <louisdelrio@outlook.com>
2016-11-21 04:38:47 -0400

    refactored input validation code into a single function.

--- src/infixrpnconv.c
+++ src/infixrpnconv.c
@@ -3,19 +3,17 @@
 #include "infixrpnconv.h"
 #include "stackstring.h"
 
+int validateInput(const char *originalExpression, const char *validInputChars);
+
 int infixToRPN(const char *originalExpression, char *outputExpression, size_t outBufferSz) {
     int ret = 0;
     memset(outputExpression, 0, outBufferSz);
     int expressionLength = strlen(originalExpression);
     if (outBufferSz >= (expressionLength + 1)) {
         const char validInputChars[] = "qwertyuioplkjhgfdsazxcvbnm^/*-+()";
-        for (int j = 0; j != expressionLength; j++) {
-            char currentTokenString[2];
-            sprintf(currentTokenString, "%c", originalExpression[j]);
-            if (!strstr(validInputChars, currentTokenString)) {
-                ret = 0x01<<6;
-                break;
-            }
+        ret = validateInput(originalExpression, validInputChars);
+        if (ret == 0) {
+
         }
     }
     else
@@ -29,16 +27,26 @@ int rpnToInfix(const char *originalExpression, char *outputExpression, size_t ou
     int expressionLength = strlen(originalExpression);
     if (outBufferSz > (expressionLength + 1)) {
         const char validInputChars[] = "qwertyuioplkjhgfdsazxcvbnm^/*-+";
-        for (int j = 0; j != expressionLength; j++) {
-            char currentTokenString[2];
-            sprintf(currentTokenString, "%c", originalExpression[j]);
-            if (!strstr(validInputChars, currentTokenString)) {
-                ret = 0x01<<6;
-                break;
-            }
+        ret = validateInput(originalExpression, validInputChars);
+        if (ret == 0) {
+
         }
     }
     else
         ret = 0x01<<7;
+    return ret;
+}
+
+int validateInput(const char *originalExpression, const char *validInputChars) {
+    int ret = 0;
+    int expressionLength = strlen(originalExpression);
+    for (int j = 0; j != expressionLength; j++) {
+        char currentTokenString[2];
+        sprintf(currentTokenString, "%c", originalExpression[j]);
+        if (!strstr(validInputChars, currentTokenString)) {
+            ret = 0x01<<6;
+            break;
+        }
+    }
     return ret;
 }
